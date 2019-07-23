41542af2eb66350b52f6b25e4ae569e4d4d26053
Intern <>
2019-07-23 17:23:29 +1000

    Merge branch 'master' of https://github.com/Teja77/LiminalExperience
    # Conflicts:
    #	Assets/particle systems/PS_orb/orb.unity

--- Assets/scripts/samplecollision.cs
+++ Assets/scripts/samplecollision.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class samplecollision : MonoBehaviour
+
 {
+
+    public GameObject Activate;
     // Start is called before the first frame update
     void Start()
     {
-
+        Activate.gameObject.SetActive(false);
+        Debug.Log(Activate.name + "deactivated");
     }
 
     // Update is called once per frame
-    void Update()
-    {
 
-    }
-    private void OnParticleCollision(GameObject other)
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name + "entered");
+        if (other.CompareTag("player"))
+        {
+            Activate.gameObject.SetActive(true);
+            gameObject.transform.tag = "destroy";
+
+        }
     }
 }
+

050f0c7eb9f65b7c1d60efc328873724d40bc02c
Teja77 <saiteja9440@gmail.com>
2019-07-26 17:00:49 +1000

    no message

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.Core;
+using Liminal.SDK.Input;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+using Liminal.Core.Fader;
+
+public class PlayerController : MonoBehaviour
+{
+    public IVRInputDevice Device;
+    public LayerMask LayerToHit;
+    public float FadeTime;
+
+    private Coroutine _teleportRoutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Device = VRDevice.Device.PrimaryInputDevice;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Device.GetButtonDown(VRButton.One))
+         {
+            MoveRaycast();
+        }
+    }
+
+    private void MoveRaycast()
+    {
+        var pointer = Device.Pointer;
+
+        if (Physics.Raycast(pointer.Transform.position, pointer.Transform.forward, out RaycastHit hit, 100f, LayerToHit))
+        {
+            if (_teleportRoutine != null)
+                return;
+
+            _teleportRoutine = StartCoroutine(TeleportCoro(hit.point));
+        }
+    }
+
+
+    private IEnumerator TeleportCoro(Vector3 teleportPoint)
+    {
+        ScreenFader.Instance.FadeToBlack(FadeTime);
+
+        yield return ScreenFader.Instance.WaitUntilFadeComplete();
+
+        transform.position = teleportPoint;
+        ScreenFader.Instance.FadeToClear(FadeTime);
+
+        _teleportRoutine = null;
+    }
+}

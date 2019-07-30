f661303e3239c8483e0514f1b6cc3aed6941d93c
Intern <>
2019-07-30 17:19:26 +1000

    rotation of game object from oculus go controller and rain effect

---
+++ Assets/scripts/Water/gravityModifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityModifier : MonoBehaviour
+{
+    private ParticleSystem ps;
+    public float gravityValue = 0.0f;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        var main = ps.main;
+        main.simulationSpeed = gravityValue;
+    }
+}
---
+++ Assets/scripts/Water/handRotation.cs
@@ -0,0 +1,24 @@
+﻿using Liminal.SDK.VR.Avatars;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handRotation : MonoBehaviour
+{
+    public VRAvatarHand Hand;
+    // Update is called once per frame
+   
+    
+    public GameObject ObjectToRotate;
+
+    void Update()
+    {
+        var handRotation = Hand.Anchor.rotation;
+
+        /*
+        var primaryHand = VRAvatar.Active.PrimaryHand;
+        primaryHand.Anchor.Rotation
+        */
+        ObjectToRotate.transform.rotation = handRotation;
+    }
+}

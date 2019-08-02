e8caf0b03629901fcbaf9007d887758495a5db8c
Intern <>
2019-08-02 17:24:22 +1000

    snow effect shader + ability to modify the amount of snow on particle collision ( working )

---
+++ Assets/scripts/Water/Snow2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Snow : MonoBehaviour
+{
+    public Material Materialtoadd;
+    public float minValue;
+    public float maxValue;
+    public float SnowAddAmount;
+    public float multiplier = 1f;
+
+    private float _snowValue;
+
+    private void Start()
+    {
+        Materialtoadd.SetFloat("_SnowAmount", _snowValue / maxValue);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        //add any other code here
+        _snowValue += SnowAddAmount;
+        _snowValue = Mathf.Clamp(_snowValue, minValue, maxValue);
+        var normalisedVal = _snowValue / maxValue;
+        normalisedVal *= multiplier;
+        Materialtoadd.SetFloat("_SnowAmount", normalisedVal);
+        Debug.Log("My Player’s been hit by particles.");
+    }
+}
---
+++ Assets/scripts/Water/snow1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class snow1 : MonoBehaviour
+{
+    public Material Materialtoadd;
+    public float minValue;
+    public float maxValue;
+    public float _snowValue = 1f;
+  
+
+    private void Start()
+    {
+        Materialtoadd.SetFloat("Vector1_D5319A04", minValue / maxValue);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        //add any other code here
+        _snowValue += 1;
+        _snowValue = Mathf.Clamp(_snowValue, minValue, maxValue);
+        
+        Materialtoadd.SetFloat("Vector1_D5319A04", _snowValue / maxValue);
+        Debug.Log("My Player’s been hit by particles.");
+    }
+}

a45114311bd10422b678be69092a4af9a363def5
Teja77 <saiteja9440@gmail.com>
2019-08-21 17:14:56 +1000

    worked on noise asset+ bumped up the visual appeal on rain system + lightning+ snow + added rotation and drag values for the snowPS so when the snow reaches near the ground it stops rotation and slows down simulation speed.

--- Assets/scripts/Water/Lightning.cs
+++ Assets/scripts/Water/Lightning.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Lightning : MonoBehaviour
 {
+    public GameObject lit;
     // Start is called before the first frame update
     void Start()
     {
       
     }
 
+   
+
     private void OnParticleCollision(GameObject other)
     {
         if(other.layer == 10)
         {
             //Destroy(other.gameObject);
-            GetComponent<AudioSource>().Play(); 
+            GetComponent<AudioSource>().Play();
+            //lit.gameObject.GetComponent<Animator>().SetTrigger("Lightning");
+
 
             
         }

249095d6fc5557bf60d18184463fb81dfb0de242
Teja77 <saiteja9440@gmail.com>
2019-08-28 17:31:53 +1000

    worked on issue which caused after developing the shader working with multiple textures (very high fram rate drops :: issue) fixed it and started experimenting with the wetness amount we can add to the ground the sky shader is working fine but have to customise it to work with tornado by making it twirl around the center.

--- Assets/scripts/Water/Snow2.cs
+++ Assets/scripts/Water/Snow2.cs
@@ -11,12 +11,14 @@ public class Snow2 : MonoBehaviour
     public float SnowDelta;
     public float multiplier = 1f;
     public float MaxSnowAmount;
+   
     public AudioSource Source;
     public float AudioDelay;
     public bool IsPlayingAudio;
 
     private Coroutine _audioRoutine;
     private float _curSnowVal;
+
 
     private void OnEnable()
     {
@@ -52,7 +54,9 @@ public class Snow2 : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         _curSnowVal = Material.GetFloat("Vector1_D5319A04");
+      
         _curSnowVal = Mathf.Clamp(_curSnowVal + (SnowDelta* multiplier), 0f, MaxSnowAmount);
         Material.SetFloat("Vector1_D5319A04", _curSnowVal);
+       
     }
 }
---
+++ Assets/scripts/Water/Wetness.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wetness : MonoBehaviour
+{
+    public Material Material;
+    public float SnowDelta;
+    public float multiplier = 1f;
+    public float MaxRainAmount;
+    private float _curRainVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        _curRainVal = Material.GetFloat("Vector1_2E42AE60");
+        _curRainVal = Mathf.Clamp(_curRainVal + (SnowDelta * multiplier), 0f, MaxRainAmount);
+        Material.SetFloat("Vector1_2E42AE60", _curRainVal);
+    }
+}
